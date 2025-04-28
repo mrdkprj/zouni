d37f7f503108560b071e34041ace9ba7549b12dd
mrdkprj <rockfield.d.m@gmail.com>
2025-04-28 18:16:21 +0900

    win device type

--- src/platform/windows/device.rs
+++ src/platform/windows/device.rs
@@ -1,12 +1,17 @@
+use super::util::decode_wide;
 use once_cell::sync::Lazy;
 use serde::{Deserialize, Serialize};
 use std::sync::Mutex;
-use windows::Win32::{
-    Devices::DeviceAndDriverInstallation::{
-        CM_Register_Notification, CM_Unregister_Notification, CM_NOTIFY_ACTION, CM_NOTIFY_ACTION_DEVICEINTERFACEARRIVAL, CM_NOTIFY_ACTION_DEVICEINTERFACEREMOVAL, CM_NOTIFY_EVENT_DATA,
-        CM_NOTIFY_FILTER, CM_NOTIFY_FILTER_FLAG_ALL_INTERFACE_CLASSES, CM_NOTIFY_FILTER_TYPE_DEVICEINTERFACE, CR_SUCCESS, HCMNOTIFICATION,
+use windows::{
+    core::{Error, GUID, PCWSTR},
+    Win32::{
+        Devices::DeviceAndDriverInstallation::{
+            CM_Register_Notification, CM_Unregister_Notification, SetupDiEnumDeviceInfo, SetupDiGetClassDevsW, SetupDiGetDeviceRegistryPropertyW, SetupDiOpenDeviceInterfaceW, CM_NOTIFY_ACTION,
+            CM_NOTIFY_ACTION_DEVICEINTERFACEARRIVAL, CM_NOTIFY_ACTION_DEVICEINTERFACEREMOVAL, CM_NOTIFY_EVENT_DATA, CM_NOTIFY_FILTER, CM_NOTIFY_FILTER_FLAG_ALL_INTERFACE_CLASSES,
+            CM_NOTIFY_FILTER_TYPE_DEVICEINTERFACE, CR_SUCCESS, DIGCF_DEVICEINTERFACE, DIODI_NO_ADD, HCMNOTIFICATION, SPDRP_CLASS, SP_DEVICE_INTERFACE_DATA, SP_DEVINFO_DATA,
+        },
+        Foundation::{ERROR_SUCCESS, MAX_PATH},
     },
-    Foundation::ERROR_SUCCESS,
 };
 
 static CONFIG: Lazy<Mutex<isize>> = Lazy::new(|| Mutex::new(0));
@@ -42,23 +47,56 @@ unsafe extern "system" fn on_notify<F: FnMut(DeviceEvent)>(
     _eventdatasize: u32,
 ) -> u32 {
     match action {
-        CM_NOTIFY_ACTION_DEVICEINTERFACEARRIVAL => {
+        CM_NOTIFY_ACTION_DEVICEINTERFACEARRIVAL | CM_NOTIFY_ACTION_DEVICEINTERFACEREMOVAL => {
+            let data = &*eventdata;
+            if data.FilterType != CM_NOTIFY_FILTER_TYPE_DEVICEINTERFACE {
+                return 0;
+            }
             let callback = &mut *(context as *mut F);
+            let name = get_device_type(data.u.DeviceInterface.ClassGuid, data.u.DeviceInterface.SymbolicLink.as_ptr()).unwrap_or_default();
             callback(DeviceEvent {
-                name: { *eventdata }.u.DeviceInterface.ClassGuid.to_u128().to_string(),
-                event: "Added".to_string(),
-            })
-        }
-        CM_NOTIFY_ACTION_DEVICEINTERFACEREMOVAL => {
-            let callback = &mut *(context as *mut F);
-            callback(DeviceEvent {
-                name: { *eventdata }.u.DeviceInterface.ClassGuid.to_u128().to_string(),
-                event: "Removed".to_string(),
+                name,
+                event: if action == CM_NOTIFY_ACTION_DEVICEINTERFACEARRIVAL {
+                    "Added".to_string()
+                } else {
+                    "Removed".to_string()
+                },
             })
         }
         _ => {}
     };
     ERROR_SUCCESS.0
+}
+
+fn get_device_type(guid: GUID, symbolic_link: *const u16) -> Result<String, Error> {
+    if let Ok(info) = unsafe { SetupDiGetClassDevsW(Some(&guid), None, None, DIGCF_DEVICEINTERFACE) } {
+        let mut device_interface_data = SP_DEVICE_INTERFACE_DATA {
+            cbSize: size_of::<SP_DEVICE_INTERFACE_DATA>() as u32,
+            ..Default::default()
+        };
+
+        unsafe { SetupDiOpenDeviceInterfaceW(info, Some(&PCWSTR::from_raw(symbolic_link)), DIODI_NO_ADD, Some(&mut device_interface_data)) }?;
+
+        let mut data = SP_DEVINFO_DATA {
+            cbSize: size_of::<SP_DEVINFO_DATA>() as _,
+            ..Default::default()
+        };
+        unsafe { SetupDiEnumDeviceInfo(info, 0, &mut data) }?;
+
+        let mut buffer = vec![0u8; MAX_PATH as _];
+        let mut property_size = 0u32;
+        unsafe { SetupDiGetDeviceRegistryPropertyW(info, &data, SPDRP_CLASS, None, Some(&mut buffer), Some(&mut property_size)) }?;
+
+        let wchar_count = property_size as usize / 2;
+        let wide_buffer = &buffer[..property_size as usize];
+        let wide_buffer = unsafe { std::slice::from_raw_parts(wide_buffer.as_ptr() as *const u16, wchar_count) };
+
+        let device_type = decode_wide(wide_buffer);
+
+        return Ok(device_type);
+    }
+
+    Ok(String::new())
 }
 
 pub fn unlisten() {
